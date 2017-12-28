7ec222f9a6b6809820013516f86e50e5b4952ed3
Sparsh Wadhwa <sparshwadhwa14@gmail.com>
2017-12-28 21:20:30 +0530

    Update MainActivity.java

--- YFHR/app/src/main/java/tech/iosd/yfhr/MainActivity.java
+++ YFHR/app/src/main/java/tech/iosd/yfhr/MainActivity.java
@@ -104,6 +104,7 @@ public class MainActivity extends AppCompatActivity
         }
         if(id==R.id.nav_chat){
             getSupportFragmentManager().beginTransaction().replace(R.id.container_frame,new chatFragment()).commit();
+            }
         if(id==R.id.nav_share) {
             Intent i = new Intent(Intent.ACTION_SEND);
             i.setType("text/plain");
